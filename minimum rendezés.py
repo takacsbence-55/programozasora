9c6f0f39a0b0445a18a6b9b469e0b49a7ee0f120
takacsbm <takacs.bence.miklos.tech2021a@bolyaimovar.com>
2023-01-26 09:48:22 +0100

    órai feladat

--- minimum rendezés.py
+++ minimum rendezés.py
@@ -1,16 +1,16 @@
-lista = [7,2,5,3,8,-5,33,6]
+lista1 = [-14,23,1,-48,-43,28,-77,-33,-95,38,-9,98,-61,58,21,87,41,-65,-22,-20,-56,-75,8,0,-77,100,67,22,8,-78,-6]
 
-for i in range(len(lista)-1):
-    print(lista)
+for i in range(len(lista1)-1):
+    print(lista1)
     minindex = i
-    for j in range(i+1,len(lista)):
-        if lista[j] < lista[minindex]:
+    for j in range(i+1,len(lista1)):
+        if lista1[j] < lista1[minindex]:
             minindex = j
             print("új minimum találat")
         else:
             print('')
     if i != minindex:
         print("csere")
-        lista[i], lista[minindex] = lista[minindex], lista[i]
+        lista1[i], lista1[minindex] = lista1[minindex], lista1[i]
 
-print(lista)
+print(lista1)
