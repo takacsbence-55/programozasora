4a20b439e1c07f5b44fee73eb7d081a7b0e32134
takacsbence-55 <114336987+takacsbence-55@users.noreply.github.com>
2023-02-16 08:01:42 +0100

    órai

--- Takács_Bence_röpdolgozat_fuggvenyek.py
+++ Takács_Bence_röpdolgozat_fuggvenyek.py
@@ -1,16 +1,30 @@
-lista = [1,9,-6,11,7,12,120,-96,55,42,300,15,-1]
-
-def novekvo_sorrend(lista):
+def novekvosorrend(lista):
     for i in range(len(lista)-1):
-        for j in (i+1, len(lista)):
-            if lista[i] > lista[j]:
+        for j in (i+1,len(lista)):
+            if lista[i] < lista[j]:
                 lista[i], lista[j] = lista[j], lista[i]
-
-            else:
-                if lista[i] > lista[j]:
-                    lista[i], lista[j] = lista[j], lista[i]
 
     return lista
 
-sorrend = novekvo_sorrend(lista)
-print(sorrend)
+def minErtek(lista):
+    minimum = lista[0]
+    for i in lista:
+        if i < minimum:
+            minimum = i
+
+    return minimum
+
+def maxErtek(lista):
+    maximum = lista[0]
+    for i in lista:
+        if i > maximum:
+            maximum = i
+
+    return maximum
+
+
+lista = [1,9,-6,11,7,12,120,-96,55,42,300,15,-1]
+lista = novekvosorrend(lista)
+print(lista)
+print(minErtek(lista))
+print(maxErtek(lista))
