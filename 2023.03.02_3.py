45d1f46c39e625ed7deadcb19a2b170eba8dc1a5
takacsbm <takacs.bence.miklos.tech2021a@bolyaimovar.com>
2023-03-02 09:35:37 +0100

    szótárak

---
+++ 2023.03.02_3.py
@@ -0,0 +1,18 @@
+konyvek = {}
+while True:
+    cimek = []
+    szerzo = input()
+
+    if szerzo == "":
+        break
+    else:
+        while True:
+            cim = input()
+            if cim == "":
+                break
+            else:
+                cimek.append(cim)
+        konyvek[szerzo] = cimek
+
+for i,j in konyvek.items():
+    print(i," : ",j)
