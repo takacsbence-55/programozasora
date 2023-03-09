e3c2ab7df0a494a4abfee3390361646a8cba98d2
takacsbm <takacs.bence.miklos.tech2021a@bolyaimovar.com>
2023-03-09 08:36:11 +0100

    szótárak

--- 2023.03.02_3.py
+++ 2023.03.02_3.py
@@ -17,8 +17,12 @@ while True:
                 cimek[cim] = {"Kiadási év":kiadas,"kategória":kategoria,"Tartalom":leiras}
         konyvek[szerzo] = cimek
 
-for i,j in konyvek.items():
-    print(i," : ",j)
+for szerzo,cimek in konyvek.items():
+    print("Szerző: ",szerzo)
+    for cim,adatok in cimek.items():
+        print("\t könyv: ",cim,)
+        for Adat_kulcs,Adat_érték in adatok.items():
+            print("\t\t",Adat_kulcs,Adat_érték)
 
 #for i in konyvek.keys():
     #print(i," : ",konyvek[i])
